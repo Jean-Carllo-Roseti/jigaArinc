905d253ba6bda64dde6c51e6f02b9256b3f97791
Jean Carllo <jcmar.dev@gmail.com>
2025-04-16 09:09:31 -0300

    testando a conversão, os valores devem ser inseridos invertidos tbm.

--- mockData.js
+++ mockData.js
@@ -1,22 +1,28 @@
-// mockData.js mudança de label os primeiros bits, direita*
+// mockData.js mudança de label os primeiros bits, direita, a mensagem vai ser invertida, procolo arinc*
 const mockARINCData = [
     {
-      binary: '101101101100110011001100010110110',
-      timestamp: Date.now()
+      binary: '101101101100110011001100001101101', 
+      timestamp: Date.now() 
+    },  
+    {
+      binary: '101101111101010101010101011101101',
+      timestamp: Date.now() 
     },
     {
-      binary: '101101111101010101010101010110111',
-      timestamp: Date.now()
+      binary: '111110111101010101010101000011101',
+      timestamp: Date.now() 
     },
     {
-      binary: '111110111101010101010101010111000',
-      timestamp: Date.now()
-    },
-    {
-      binary: '0111101111010101010101010010111001',
-      timestamp: Date.now()
+      binary: '011110111101010101010101010011101',
+      timestamp: Date.now() 
       },
   ];
-  
+
   module.exports = { mockARINCData };
-  
+
+// 10110110 = 266 inserir esse valores invertidos.
+// 10110111 = 267
+// 10111000 = 270
+// 10111001 = 271
+// 10111011 = 273
+// 10111100 = 274
